7c65713e0d5188054f72f4fef86574b1a0141f17
Sebastian <sebastiangohkaijun@gmail.com>
2026-09-04 10:57:33 +0800

    Activity 2

---
+++ Lab_1.py
@@ -0,0 +1,22 @@
+print("Welcome here")
+print("My first post")
+
+username = "hello"
+bio = "test"
+followers = 1
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
+
+followers += 50
+
+print("Day 1:", followers)
+
+followers += 50
+
+print("Day 2:", followers)
+
+followers += 50
+
+print("Day 3:", followers)
