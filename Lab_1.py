90b5dd37d9015b88c6c1b82a8d747d68e75652f4
Sebastian <sebastiangohkaijun@gmail.com>
2026-09-18 10:04:36 +0800

    add dockerfile

--- Lab_1.py
+++ Lab_1.py
@@ -1,3 +1,41 @@
+
+#Activity 2
+username = "test"
+bio = "hi"
+
+
+print ("Username: ", username)
+print ("Bio: ", bio)
+
+
+#Activity 3
+followers = 500
+
+followers += 100
+
+print("Day 1: ", followers)
+
+followers += 100
+
+print("Day 2: ", followers)
+
+followers += 100
+
+print("Day 3: ", followers)
+
+#Activity 4
+username = input("Enter username: ")
+bio = input("Enter bio: ")
+age = int(input("Age: "))
+category = input("Enter category: ")
+
+print ("Username: ", username)
+print ("Bio: ", bio)
+print("Age: ", age)
+print ("Category: ", category)
+
+
+#Activity 5
 username = input("Enter username: ")
 bio = input("Enter bio: ")
 age = int(input("Age: "))
