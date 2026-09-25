99c0b63882b410cba00cb175370a19a24b3c2d6e
sebastiangohkaijun@gmail.com <sebastian-goh@Sebastians-MacBook-Air.local>
2026-09-25 11:01:29 +0800

    finalise dockerfile and main code

--- Lab-4/persistent_auditor.py
+++ Lab-4/persistent_auditor.py
@@ -4,6 +4,14 @@ def save_inventory(orders: list):
         for i in range(len(orders)):
             file.write(f"{i+1}: {orders[i]}\n")
 
+def load_inventory():
+    try:
+        with open("inventory.txt", "r") as file:
+            print("\n\nLoading inventory from inventory.txt...")
+            print(f"\n{file.read()}")
+    except FileNotFoundError:
+        print("No inventory found. Starting a new inventory.")
+
 def get_valid_input(entry):
     if str(entry).isdigit() == False:
         if entry == "quit":
@@ -35,3 +43,4 @@ while (True):
 
 save_inventory(orders)
 print("Orders saved to inventory.txt")
+load_inventory()
